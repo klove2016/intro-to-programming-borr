c75d0a6134dd8c47d46f26a133a69445b4c9843a
KJ <KJLOVING1400@GMAIL.COM>
2022-08-02 18:21:36 -0800

    essageadded comment form

--- js/index.js
+++ js/index.js
@@ -14,8 +14,56 @@ const skills = ["JS", "HTML", "CSS"];
 const skillsSection = document.getElementById("mySkills");
 const skillsList = skillsSection.querySelector("ul");
 skills.forEach((skill) => {
-    const skillItem = document.createElement("li")
-    skillItem.innerText= skill
-    skillsList.appendChild(skillItem)
-})
+  const skillItem = document.createElement("li");
+  skillItem.innerText = skill;
+  skillsList.appendChild(skillItem);
+});
 
+const messageForm = document.getElementsByName("leave_message")[0];
+
+const nameRes = document.getElementsByName("name")[0];
+
+const emailRes = document.getElementsByName("email")[0];
+
+const messageRes = document.getElementsByName("message")[0];
+
+const submitButton = document.getElementsByName("submit")[0];
+
+let submitForm = function (e) {
+  e.preventDefault();
+  clientName = nameRes.value;
+  clientEmail = emailRes.value;
+  clientMessage = messageRes.value;
+  const messageSection = document.getElementById("messages");
+
+  const messageList = messageSection.querySelector("ul");
+  const newMessage = document.createElement("li");
+
+  newMessage.innerHTML = `<a href="mailto:${clientEmail}">${clientName}</a> 
+ <span id="msg-span"> wrote: ${clientMessage}</span>`;
+
+  messageList.appendChild(newMessage);
+  const removeButton = document.createElement("button");
+  removeButton.innerText = "Remove";
+  removeButton.setAttribute("type", "button");
+  newMessage.appendChild(removeButton);
+  messageList.appendChild(newMessage);
+  console.log(newMessage);
+  messageForm.reset();
+  function removeComment() {
+    entry = removeButton.parentNode;
+    entry.remove();
+  }
+  removeButton.addEventListener("click", removeComment);
+
+  document.getElementById("messages").style.visibility = "visible";
+};
+
+submitButton.addEventListener("click", submitForm);
+
+if (
+  document.getElementById("messages").querySelector("ul").childElementCount ===
+  0
+) {
+  document.getElementById("messages").style.visibility = "hidden";
+}
